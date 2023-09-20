2f4949e769ac8a622b3cd283e443bac22e832aaa
MuhammadBilalKhan267 <130090003+MuhammadBilalKhan267@users.noreply.github.com>
2023-09-20 16:32:03 +0500

    Add files via upload

---
+++ RockPaperScissors/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Muhammad Bilal Khan - Rock Paper Scissors</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Rock Paper Scissors</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+<a href="game.php">game.php</a>
+</p>
+</div>
+</body>
